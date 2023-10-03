200fd691e1d66fb239bc53d6e167b1c8892dacae
LukeLikeDirt <L.Florence@latrobe.edu.au>
2023-10-03 17:14:05 +1100

    Updated DADA2 code

--- code/01.Bioinformatics_ITS/03.Quality_filter_denoise.R
+++ code/01.Bioinformatics_ITS/03.Quality_filter_denoise.R
@@ -1,53 +1,72 @@
-# This script performs quality filtering, denoising, and sequence table construction for paired-end 16S rRNA gene amplicon data using DADA2.
+# Script: code/01.Bioinformatics_ITS/03.Quality_filter_denoise.R
+# Purpose: Prepare Illumina forwards reads targeting the ITS1 chimera detection
+# in VSEARCH.
+# Author: Luke Florence
+# Date: 28th September 2023
+#
+# This script is adapted from: https://benjjneb.github.io/dada2/bigdata.html
+#
+# Script Purpose:
+# ---------------
+# This R script is designed for the quality filtering and denoising of
+# Illumina forward reads targeting the ITS1 subregion using DADA2. The script
+# is designed for "big data" projects, and therefore, performs quality
+# filtering and denoising on sequencing run subsets. The data are subset by
+# sequencing run to improve error rate estimations, as each sequencing run has
+# a unique error profile. The script also converts the DADA2 'rds' sequence
+# table to a 'fasta' file formatted for VESEARCH for subsequent chimera
+# detection and removal in VSEARCH.
+#
+# Script Overview:
+# ---------------
+# This script (1) quality filters reads, (2) denoises the quality-filtered
+# reads, (3) merges the denoised sequence tables, and (4) converts the merged
+# sequence table to a fasta file for chimera detection and removal in VSEARCH.
+# Note that the reads have already been trimmed using ITSxpress and quality
+# truncated using Trimmomatic.
 
-## Load DADA2
-require(dada2, lib.loc = '/data/group/frankslab/project/LFlorence/MycorrhizasAustralianForests/envs/R-packages')
+# Load required libraries
+library(dada2, lib.loc = '/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/envs/R-packages')
+library(seqinr, lib.loc = '/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/envs/R-packages')
+library(tidyverse, lib.loc = '/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/envs/R-packages')
+
+# Constant variables
+num_runs <- 2   # The number of sequencing runs that will be processed
+mEE <- 1        # maxEE: maximum number of expected errors per read
+tL <- 0         # truncLen: fixed length for truncating reads
+tQ <- 0         # truncQ: quality threshold for truncating reads
+mNs <- 0        # maxN: maximum number of Ns allowed in a read
 
 # Set the working directory to the project directory
 setwd("/data/group/frankslab/project/LFlorence/MycorrhizaAusForests")
 
-# Define the path to the directory containing the runs
+# Define the path to the ITS directory
 path <- "data/AusMicrobiome/ITS"
 
-# Define the number of sequencing runs thta will be processed
-num_runs <- 44
-
-# Define the parameters for quality filtering
-mEE <- 1      # maxEE: maximum number of expected errors per read
-tL <- 0       # truncLen: fixed length for truncating reads
-tQ <- 0       # truncQ: quality threshold for truncating reads
-mNs <- 0      # maxN: maximum number of Ns allowed in a read
-# NOTE: tL and tQ are set to 0 to because the reads are already trimmed and truncated with ITSxpress and Trimmomatic
-# NOTE: I am using a maxEE of 1, which is relativley strict when using a "denoising" workflow. However, I am employin strict quality filtering because reads could not be merged.
-
-# Create subdirectories for quality filtered 'fwd' and 'rev' fastq files
-for (i in 1:num_runs) {
-  run_dir = file.path(path, '04.Quality_filtered', paste0('run', i))
-  dir.create(run_dir, recursive = TRUE) # create parent directories if they don't exist
-  dir.create(file.path(run_dir, 'fwd'))
-  dir.create(file.path(run_dir, 'rev'))
+# Create subdirectories for quality-filtered and denoised files
+for (run in 1:num_runs) {
+  run_dir <- file.path(path, '04.Quality_filtered', paste0('run', run))
+  dir.create(run_dir, recursive = TRUE) # Create parent directories if they don't exist
 }
-## Subdirectories for merged and denoised sequence tables
-for (i in 1:num_runs) {
-  run_dir = file.path(path, '05.Denoised', paste0('run', i))
-  dir.create(run_dir, recursive = TRUE) # create parent directories if they don't exist
-}
+dir.create(file.path(path, '05.Denoised'))
+
+# Create an empty data frame to store the read tracking summary
+summary_track <- data.frame()
 
 # Quality filter each run individually
 for (run in 1:num_runs) {
     
     ## Input and output directories
-    trimmed_dir <- file.path(path, paste0('03.Quality_trimmed/run', run_number, '/'))
-    qualFilt_dir <- file.path(path, paste0('04.Quality_filtered/run', run_number))
-    denoised_dir <- file.path(path, '05.Denoised')
+    trimmed_dir <- file.path(path, paste0('03.Quality_trimmed/run', run, '/'))
+    qualFilt_dir <- file.path(path, paste0('04.Quality_filtered/run', run))
     
     ## List of files to be filtered
-    fns = list.files(trimmed, pattern = 'fastq.gz')
+    fns <- list.files(trimmed_dir, pattern = 'fastq.gz')
     sample.names <- sapply(strsplit(basename(fns), "_"), `[`, 1)  ## Assumes filename = samplename_XXX.fastq.gz
     names(fns) <- sample.names
+    
     ## Quality filtering
-    # Quality filtering for the current run
-    qualFilt <- filterAndTrim(fns, file.path(qualFilt_dir, basename(fns)),
+    qualFilt <- filterAndTrim(file.path(trimmed_dir, fns), file.path(qualFilt_dir, basename(fns)),
                               maxEE = mEE, truncLen = tL, truncQ = tQ, maxN = mNs,
                               rm.phix = TRUE, compress = TRUE, verbose = TRUE, multithread = TRUE,
                               matchIDs = TRUE)
@@ -55,47 +74,105 @@ for (run in 1:num_runs) {
     # Track reads through the quality filtering pipeline
     colnames(qualFilt) <- c("input_qualFilter", "output_qualFilter")
     rownames(qualFilt) <- sample.names
+    
     # Save track file to denoised directory
     write.csv(qualFilt, file = file.path(path, "04.Quality_filtered", paste0("run", run), "stats.csv"))
-
 }
 
 # Denoise each run individually
 for (run in 1:num_runs) {
 
-    # Define the input and output directories for the current run
-    qualFilt <- file.path(path, "03.Quality_filtered", paste0("run", run))
-    dnoise <- file.path(path, "04.Denoised", paste0("run", run))
+    ## Input and output directories
+    qualFilt_dir <- file.path(path, "04.Quality_filtered", paste0("run", run))
+    denoised_dir <- file.path(path, "05.Denoised")
 
     ## Denoising
-    filts = list.files(qualFiltered, pattern = 'fastq.gz', full.names = T)
-    sample.names = sapply(strsplit(basename(filts), '_'), `[`, 1)
-    names(filts) = sample.names
+    filts <- list.files(qualFilt_dir, pattern = 'fastq.gz', full.names = TRUE)
+    sample.names <- sapply(strsplit(basename(filts), '_'), `[`, 1)
+    names(filts) <- sample.names
     
     ## Learn error rates
     set.seed(1986)
-    err = learnErrors(filts, nbases = 1e8, multithread = T, randomize = T)
+    err <- learnErrors(filts, nbases = 1e8, multithread = TRUE, randomize = TRUE)
     
     ## Infer sequence variants
-    dds = vector('list', length(sample.names))
-    names(dds) = sample.names
+    dds <- vector('list', length(sample.names))
+    names(dds) <- sample.names
     for (sam in sample.names) {
         cat('Processing:', sam, '\n')
-        derep = derepFastq(filts[[sam]])
-        dds[[sam]] = dada(derep, err = err, multithread = TRUE)
+        derep <- derepFastq(filts[[sam]])
+        dds[[sam]] <- dada(derep, err = err, multithread = TRUE)
     }
     
     ## Build sequence table
-    seqtab = makeSequenceTable(dds)
-    saveRDS(seqtab, file.path(path, paste0('04.Denoised/run', run_number, '_seqtab.rds')))
+    seqtab <- makeSequenceTable(dds)
+    saveRDS(seqtab, file.path(denoised_dir, paste0(run, '_seqtab.rds')))
 
-    # Track reads through the DADA2 pipeline
-    quality <- read.csv(file.path(qualFilt, "stats.csv"))
+    # Track reads through the DADA2 pipeline for the current run
+    quality <- read.csv(file.path(qualFilt_dir, "stats.csv"))
     getN <- function(x) sum(getUniques(x))
-    track <- cbind(quality, sapply(dds, getN))
-    # If processing a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
-    colnames(track) <- c("sample_names", "input_qualFilter", "output_qualFilter", "output_denoised")
-    # Save track file to denoised directory
-    write.csv(track, file = file.path(dnoise, "stats.csv"), row.names = FALSE)
-  
+    
+    # Check if there are rows in the "stats.csv" file
+    if (nrow(quality) > 0) {
+        track <- data.frame(
+            run_number = run,
+            input_qualFilter = quality$input_qualFilter,
+            output_qualFilter = quality$output_qualFilter,
+            output_denoised = sapply(dds, getN)
+        )
+        
+        # Append the track information for the current run to the summary_track data frame
+        summary_track <- rbind(summary_track, track)
+    }
 }
+
+# Track reads across the pipeline, summarized by sequencing run
+summary_track %>%
+    rename("Run" = run_number) %>%
+    group_by(Run) %>%
+    summarise(
+        Input_Reads = sum(input_qualFilter),
+        Output_Reads = sum(output_qualFilter),
+        "Retained_Reads_%" = round((sum(output_denoised) / sum(input_qualFilter)) * 100, digits = 2),
+        "Dropped_Reads_%" = round((sum(input_qualFilter) - sum(output_qualFilter)) / sum(input_qualFilter), digits = 2),
+        Mean_Reads_Per_Sample = round(sum(output_qualFilter) / n(), digits = 0),
+        Min_Reads = min(output_qualFilter),
+        Max_Reads = max(output_qualFilter),
+        Samples_LT_10K_Reads = sum(output_qualFilter < 10000),
+        "Samples_10-20K_Reads" = sum(output_qualFilter >= 10000 & output_qualFilter < 20000),
+        Samples_MT_20K_Reads = sum(output_qualFilter >= 20000)) %>%
+        ungroup() %>%
+        write.csv(file.path(path, "denoise_summary.csv"), row.names = FALSE)
+
+# Merge all sequence tables and convert rds to fasta format for chimera detection in VSEARCH
+
+# Initialise an empty list to store the sequence tables
+seqtab_list <- list()
+
+# Loop through the sequencing runs to merge the sequence tables
+for (run in 1:num_runs) {
+  seqtab_file <- file.path(path, paste0('05.Denoised/', run, '_seqtab.rds'))
+  seqtab <- readRDS(seqtab_file)
+  seqtab_list[[run]] <- seqtab
+}
+
+# Merge all sequence tables
+all.seqtab <- do.call(mergeSequenceTables, seqtab_list)
+
+## Convert rds to fasta for chimera detection in VSEARCH
+
+## Format data frame
+fasta.tab <- as.data.frame(all.seqtab) %>%
+    rownames_to_column('sample.names') %>%
+    as_tibble() %>%
+    pivot_longer(-sample.names, names_to = 'seq', values_to = 'size') %>%
+    filter(size > 0) %>%
+    ungroup() %>%
+    mutate(seq.name = paste0(sample.names, '.fasta', '.', row_number(), ';size=', size)) %>%
+    select(seq.name, seq = seq)
+
+## Save the rds file for the merged sequence table
+saveRDS(all.seqtab, file.path(path, '05.Denoised/all_seqtab.rds'))
+
+## Write and save the fasta file for the merged sequence table
+write.fasta(as.list(fasta.tab$seq), fasta.tab$seq.name, file.path(path, '05.Denoised/all.fasta'), open = 'w', nbchar = 60, as.string = FALSE)
--- code/01.Bioinformatics_ITS/03.Quality_filter_denoise.sh
+++ code/01.Bioinformatics_ITS/03.Quality_filter_denoise.sh
@@ -3,18 +3,22 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=168:00:00
 #SBATCH --partition=week
-#SBATCH --mem-per-cpu=32G
+#SBATCH --mem=100G
+#SBATCH --output=/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/code/01.Bioinformatics_ITS/slurm/%x.%j.out
 
-printf "Starting at: $(date)"
+## The associated R script used here is adapted from: https://benjjneb.github.io/dada2/bigdata.html
+## See the R script for details on script purpose.
+
+printf "\nStarting at: %s\n" "$(date)"
 
 ## Activate the R environment
-source /data/group/frankslab/home/21258990/mambaforge/etc/profile.d/conda.sh    # Path to conda environment
+source /data/group/frankslab/home/21258990/mambaforge/etc/profile.d/conda.sh
 conda activate R
 
 # Run Rscript in a clean R instance, output a logfile
-R_SCRIPT_PATH="/data/group/frankslab/project/LFlorence/MycorrhizasAustralianForests/code/01.Bioinformatics_ITS/03.Quality_filter_denoise.R"
-LOG_FILE="slurm-${SLURM_JOBID}.Rout"
-OUTPUT_LOG="slurm-${SLURM_JOBID}.out"
+R_SCRIPT_PATH="/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/code/01.Bioinformatics_ITS/03.Quality_filter_denoise.R"
+LOG_FILE="/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/code/01.Bioinformatics_ITS/slurm/03.Quality_filter_denoise.sh.${SLURM_JOBID}.Rout"
+OUTPUT_LOG="/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/code/01.Bioinformatics_ITS/slurm/03.Quality_filter_denoise.sh.${SLURM_JOBID}.out"
 
 Rscript --vanilla --verbose "$R_SCRIPT_PATH" > "$LOG_FILE" 2>&1 
 
@@ -27,4 +31,4 @@ rm "$LOG_FILE"
 ## Deactivate the R environment
 conda deactivate
 
-printf  "Finished at: $(date)"
+printf "\nFinished at: %s\n" "$(date)"
