cf7f4892fad3e70eb1c5b9f2342d529f8225c6ee
LukeLikeDirt <L.Florence@latrobe.edu.au>
2023-10-05 12:26:39 +1100

    Initial clustering code

--- code/01.Bioinformatics_ITS/06.Cluster_OTUs.sh
+++ code/01.Bioinformatics_ITS/06.Cluster_OTUs.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=168:00:00
-#SBATCH --partition=week
+#SBATCH --time=24:00:00
+#SBATCH --partition=day
 #SBATCH --output=/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/code/01.Bioinformatics_ITS/slurm/%x.%j.out
 
+## Script: Cluster OTUs.
+## Purpose: To cluster OTUs prior to the selection of OTU representative sequences.
+## Credit: This script is adapted from https://github.com/torognes/vsearch/wiki/VSEARCH-pipeline.
+## Author: Luke Florence.
+## Date: 5th October 2023.
+
 ## Constants and file paths
-readonly THREADS=8                                                                      ## Set the number of threads
-readonly IDENTITY=0.97                                                                  ## Set the identity threshold for clustering
-readonly path="/data/group/frankslab/project/LFlorence/MycorrhizaAusForests"            ## Path to the project directory
-readonly chimeraFiltered_dir="$path/data/AusMicrobiome/ITS/test/07.Chimera_filtered"    ## Path to chimera filtered fasta file
-readonly clustered_dir="$path/data/AusMicrobiome/ITS/test/08.Clustered"                 ## Path to clustered fasta file and OTU table
-readonly output_dir="$path/output/"                                                     ## Path to formatted OTU table
-mkdir -p "$clustered_dir"                                                               ## Make the clustered subdirectory
-mkdir -p "$output_dir"                                                                  ## Make the clustered subdirectory
+readonly THREADS=8                                                                       ## Set the number of threads
+readonly IDENTITY=0.97                                                                   ## Set the identity threshold for clustering
+readonly PROJECT_PATH="/data/group/frankslab/project/LFlorence/MycorrhizaAusForests"     ## Path to the project directory
+readonly CHIMERA_FILTERED_DIR="$PROJECT_PATH/data/AusMicrobiome/ITS/07.Chimera_filtered" ## Path to chimera filtered fasta file
+readonly CLUSTERED_DIR="$PROJECT_PATH/data/AusMicrobiome/ITS/08.Clustered"               ## Path to clustered fasta file and OTU table
+readonly OUTPUT_DIR="$PROJECT_PATH/output/"                                              ## Path to formatted OTU table
+mkdir -p "$CLUSTERED_DIR"                                                                ## Make the clustered subdirectory
+mkdir -p "$OUTPUT_DIR"                                                                   ## Make the clustered subdirectory
 
 ## Log function
 log() {
@@ -22,44 +28,46 @@ log() {
     printf "\n%s %s\n\n" "$1" "$timestamp"
 }
 
-## Function for clustering at 97% and formatting the OTU table
-cluster_and_format_otu_table() {
+## Function for clustering OTUs and formatting the OTU table
+cluster_OTUs() {
 
     ## Cluster at 97% and generate OTU table
     log 'Clustering at 97% at:'
 
     vsearch \
-        --cluster_size "$chimeraFiltered_dir/all.nonchimeras.derep.fasta" \
+        --cluster_size "$CHIMERA_FILTERED_DIR/all.nonchimeras.derep.fasta" \
         --threads "$THREADS" \
         --id "$IDENTITY" \
         --strand plus \
         --sizein --sizeout \
         --fasta_width 0 \
-        --uc "$chimeraFiltered_dir/all.clustered.uc" \
+        --uc "$CHIMERA_FILTERED_DIR/all.clustered.uc" \
         --relabel_sha \
-        --centroids "$clustered_dir/OTUs.fasta" \
-        --otutabout "$clustered_dir/OTUs.txt"
+        --centroids "$CLUSTERED_DIR/OTUs.fasta" \
+        --otutabout "$CLUSTERED_DIR/OTUs.txt"
 
     ## Format OTU table
     ## Rename the header in the file
-    sed -i '1s/#OTU ID/OTU_ID/' "$clustered_dir/OTUs.txt"
+    sed -i '1s/#OTU ID/OTU_ID/' "$CLUSTERED_DIR/OTUs.txt"
     ## Convert to .csv and save to the output directory
-    sed -e 's/\s\+/,/g' "$clustered_dir/OTUs.txt" > "$output_dir/OTUs.csv"
+    sed -e 's/\s\+/,/g' "$CLUSTERED_DIR/OTUs.txt" > "$OUTPUT_DIR/OTUs.csv"
 
-    printf '\nNumber of OTUs and reads:\n'
-    printf '    Number of OTUs: %s\n' "$(grep -c "^>" "$clustered_dir/OTUs.fasta")"
-    printf '    Number of reads: %s\n' "$(grep -c "^>" "$chimeraFiltered_dir/all.nonchimeras.derep.fasta")"
+    printf '\nNumber of OTUs and reads\n'
+    printf '    Number of OTUs: %s\n' "$(grep -c "^>" "$CLUSTERED_DIR/OTUs.fasta")"
+    printf '    Number of reads: %s\n' "$(grep -c "^>" "$CHIMERA_FILTERED_DIR/all.nonchimeras.derep.fasta")"
 
 }
 
-## Execute the cluster_and_format_otu_table function
+###############################################################################
+## Main script ################################################################
+###############################################################################
 
 log 'Starting at:'
 
 source /data/group/frankslab/home/21258990/mambaforge/etc/profile.d/conda.sh
 conda activate shell
 
-cluster_and_format_otu_table
+cluster_OTUs
 
 conda deactivate
 
