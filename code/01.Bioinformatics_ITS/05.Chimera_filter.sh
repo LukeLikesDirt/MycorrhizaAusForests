c904198a9b74740f11b63c079dad88bbfd34dd6b
LukeLikeDirt <L.Florence@latrobe.edu.au>
2023-10-05 12:27:16 +1100

    Initial chimera filter code

--- code/01.Bioinformatics_ITS/05.Chimera_filter.sh
+++ code/01.Bioinformatics_ITS/05.Chimera_filter.sh
@@ -6,36 +6,57 @@
 #SBATCH --mem-per-cpu=32G
 #SBATCH --output=/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/code/01.Bioinformatics_ITS/slurm/%x.%j.out
 
-## This chimera filtering script is adapted from: https://github.com/torognes/vsearch/wiki/VSEARCH-pipeline
+## Script: Remove chimeras using de novo and reference-based chimera detection in VSEARCH.
+## Purpose: To remove chimeras prior to clustering and generating the OTU table.
+## Credit: This script is adapted from https://github.com/torognes/vsearch/wiki/VSEARCH-pipeline
+## Author: Luke Florence.
+## Date: 4th October 2023.
+##
+## Script Overview:
+## ---------------
+##   (1) Dereplicate across samples.
+##   (2) Denoise the reads from each sequencing run induvidually.
+##   (3) Merges the denoised sequence tables.
+##   (4) Convert the merged sequence table to a fasta file for chimera detection
+##       and removal in VSEARCH.
 ##
 ## Pre-requisites:
 ## ---------------
-## At this stage, there should be one '.fasta' file formatted for VSEARCH, 
-## containing samples from all sequencing runs. Reads should be ITS extracted,
-## quality-trimmed, quality-filtered, denoised and dereplicated within samples.
+## By this point, reads should be quality processed and dereplicated within
+## samples. If processing ITS sequences, reads should be ITS extracted.
 ##
-## Script Purpose:
-## ---------------
+## There should be one file named 'all.fasta' formatted for VSEARCH. That is,
+## each unique sequence should occupy two lines, formatted as follows:
+##  (1) <sample_name>.fasta.<unique_ID>;size=<number_of_reads>;
+##  (2) <the actual sequence>
+##
+## There should also be a mapping file named 'map.pl'. The mapping file can be
+## found in this repository or on the VSEARCH wiki linked at the top of the 
+## page. Otherwise here is a link to wiki with an example VSEARCH pipline that
+## doesn't use a map.pl file: https://github.com/torognes/vsearch/wiki/Alternative-VSEARCH-pipeline
+##
+## Additional notes:
+## ----------------
 ## Here, I will dereplicate across samples and perform both de-novo and
-## reference-based chimera filtering in VSEARCH. I have chosen to perform
-## chimera filtering using VSEARCH rather than DADA2 because:
-##  (1) The DADA2 de novo chimera detection has high rates of false positives
-##      compared to the UCHIME3 de novo algorithm that are implemented in
-##      VSEARCH (Edgar 2016, bioRxiv 074252; https://doi.org/10.1101/074252).
-##  (2) DADA2 does not implement reference-based chimera detection. Although
-##      de novo chimera detection outperforms reference-based chimera detection,
-##      in terms of true positive chimera detection, referenced-based chimera
-##      detection is recommended as most referenced-based chimeras are true
-##      chimeras (Tedersoo et al. 2022, Molecular ecology 31, no. 10 (2022): 2769-2795).
+## reference-based chimera filtering in VSEARCH. The previous quality and
+## denosing steps in this pipeline were done with DADA2. I have chosen to
+## swithch from DADA2 to VSEARCH to perform chimera filtering because:
+##  (1) De novo chimera detection with DADA2 results in has high rates
+##      of false positives compared to the UCHIME3 algorithm that is used
+##      by VSEARCH (Edgar 2016, bioRxiv 074252; https://doi.org/10.1101/074252).
+##  (2) VSEARCH impliments reference-based chimera detection whereas VSEARCH
+##      does not. Despite de novo chimera detection outperforming the 
+##      reference-based approach in terms of true positive detection, the
+##      referenced-based approach is a recomended suppliment as most 
+##      referenced-based chimeras are true chimeras (Tedersoo et al. 2022, Molecular ecology 31, no. 10 (2022): 2769-2795).
 
-## Constant variables and file paths
-readonly THREADS=8                                                                      ## Set the number of threads
-readonly path="/data/group/frankslab/project/LFlorence/MycorrhizaAusForests"            ## The path to the project directory
-readonly map="$path/data/AusMicrobiome/ITS/map.pl"                                      ## Map file for fasta reconstruction
-readonly refseqs="$path/data/AusMicrobiome/ITS/06.Reference_dataset/ITS1/ITS1.fasta"    ## Path to UNITE reference dataset
-readonly denoised_dir="$path/data/AusMicrobiome/ITS/05.Denoised"                        ## Path to denoised fasta file
-readonly chimeraFiltered_dir="$path/data/AusMicrobiome/ITS/07.Chimera_filtered"         ## Path for chimera filtered fasta file
-mkdir -p "$chimeraFiltered_dir"                                                         ## Chimera filtered directory
+readonly THREADS=8                                                                                  ## Set the number of threads
+readonly PROJECT_PATH="/data/group/frankslab/project/LFlorence/MycorrhizaAusForests"                ## The path to the project directory
+readonly MAP_SCRIPT="$PROJECT_PATH/data/AusMicrobiome/ITS/map.pl"                                   ## Map file for fasta reconstruction
+readonly REFERENCE_SEQS="$PROJECT_PATH/data/AusMicrobiome/ITS/06.Reference_dataset/ITS1/ITS1.fasta" ## Path to UNITE reference dataset
+readonly DENOISED_DIR="$PROJECT_PATH/data/AusMicrobiome/ITS/05.Denoised"                            ## Path to denoised fasta file
+readonly CHIMERA_FILTERED_DIR="$PROJECT_PATH/data/AusMicrobiome/ITS/07.Chimera_filtered"            ## Path for chimera filtered fasta file
+mkdir -p "$CHIMERA_FILTERED_DIR"                                                                    ## Chimera filtered directory
 
 ## Log function
 log() {
@@ -50,53 +71,57 @@ chimera_filter() {
     log 'Dereplicating across samples at:'
 
     vsearch \
-        --derep_fulllength "$denoised_dir/all.fasta" \
+        --derep_fulllength "$DENOISED_DIR/all.fasta" \
         --sizein --sizeout \
         --fasta_width 0 \
-        --uc "$chimeraFiltered_dir/all.derep.uc" \
-        --output "$chimeraFiltered_dir/all.derep.fasta"
+        --uc "$CHIMERA_FILTERED_DIR/all.derep.uc" \
+        --output "$CHIMERA_FILTERED_DIR/all.derep.fasta"
 
     log 'De novo chimera detection at:'
 
     vsearch \
-        --uchime3_denovo "$chimeraFiltered_dir/all.derep.fasta" \
+        --uchime3_denovo "$CHIMERA_FILTERED_DIR/all.derep.fasta" \
         --sizein --sizeout \
         --fasta_width 0 \
-        --nonchimeras "$chimeraFiltered_dir/all.denovo.nonchimeras.fasta"
+        --nonchimeras "$CHIMERA_FILTERED_DIR/all.denovo.nonchimeras.fasta"
 
     log 'Reference-based chimera detection at:'
 
     vsearch \
-        --uchime_ref "$chimeraFiltered_dir/all.denovo.nonchimeras.fasta" \
+        --uchime_ref "$CHIMERA_FILTERED_DIR/all.denovo.nonchimeras.fasta" \
         --threads "$THREADS" \
-        --db "$refseqs" \
+        --db "$REFERENCE_SEQS" \
         --sizein --sizeout \
         --fasta_width 0 \
-        --nonchimeras "$chimeraFiltered_dir/all.ref.nonchimeras.fasta"
+        --nonchimeras "$CHIMERA_FILTERED_DIR/all.ref.nonchimeras.fasta"
 
     ## Map reads back to dereplicated non-chimeric sequences
     log 'Extract all dereplicated non-chimeric sequences at:'
 
-    perl "$map" "$chimeraFiltered_dir/all.derep.fasta" "$chimeraFiltered_dir/all.derep.uc" "$chimeraFiltered_dir/all.ref.nonchimeras.fasta" > "$chimeraFiltered_dir/all.nonchimeras.derep.fasta"
+    perl "$MAP_SCRIPT" "$CHIMERA_FILTERED_DIR/all.derep.fasta" "$CHIMERA_FILTERED_DIR/all.derep.uc" "$CHIMERA_FILTERED_DIR/all.ref.nonchimeras.fasta" > "$CHIMERA_FILTERED_DIR/all.nonchimeras.derep.fasta"
 
     ## Track reads across the pipeline
-    printf 'Track reads across the pipeline:\n'
-    printf 'Unique sequence input: %s\n' "$(grep -c "^>" "$chimeraFiltered_dir/all.derep.fasta")"
-    printf 'Unique sequences after de novo chimera detection: %s\n' "$(grep -c "^>" "$chimeraFiltered_dir/all.denovo.nonchimeras.fasta")"
-    printf 'Unique sequences after reference-based chimera detection: %s\n' "$(grep -c "^>" "$chimeraFiltered_dir/all.ref.nonchimeras.fasta")"
-    printf 'Unique non-chimeric sequence output: %s\n' "$(grep -c "^>" "$chimeraFiltered_dir/all.nonchimeras.derep.fasta")"
+    printf 'Track reads across the pipeline\n'
+    printf '    Unique sequence input: %s\n' "$(grep -c "^>" "$CHIMERA_FILTERED_DIR/all.derep.fasta")"
+    printf '    Unique sequences after de novo chimera detection: %s\n' "$(grep -c "^>" "$CHIMERA_FILTERED_DIR/all.denovo.nonchimeras.fasta")"
+    printf '    Unique sequences after reference-based chimera detection: %s\n' "$(grep -c "^>" "$CHIMERA_FILTERED_DIR/all.ref.nonchimeras.fasta")"
+    printf '    Unique non-chimeric sequence output: %s\n' "$(grep -c "^>" "$CHIMERA_FILTERED_DIR/all.nonchimeras.derep.fasta")"
 
 }
 
-## Execute the chimera filter function
+###############################################################################
+### Main script ###############################################################
+###############################################################################
 
 log 'Starting at:'
 
 ## Activate the conda environment
 source /data/group/frankslab/home/21258990/mambaforge/etc/profile.d/conda.sh
 conda activate shell
-## Dereplicate and remove chimeras
+
+## Dereplicate across samples and remove chimeras
 chimera_filter
+
 ## Deactivate the conda environment
 conda deactivate
 
