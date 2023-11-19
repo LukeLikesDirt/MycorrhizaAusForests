37b305df113a2b6eb7f23b6bf08375f43230ba25
LukeLikeDirt <L.Florence@latrobe.edu.au>
2023-11-19 21:17:44 +1100

    Updated

--- code/01.Bioinformatics_ITS/08.Subset_samples.R
+++
@@ -1,9 +0,0 @@
-
-require(tidyverse, lib.loc = '/data/group/frankslab/project/LFlorence/MycorrhizaAusForests/envs/R-packages')
-
-setwd("/data/group/frankslab/project/LFlorence/MycorrhizaAusForests")
-
-otu <- "data/AusMicrobiome/ITS"
-
-OTUs = read.csv("data/AusMicrobiome/ITS/08.Clustered/OTUs.txt", header = T, sep = " ")
-
